6763b17583072452f5207e557cd7ad942dacb2cf
Chris Kelly <cgkelly@ucdavis.edu>
2018-06-21 18:16:56 -0700

    Increased difficulty and include start menu

--- Map.py
+++ Map.py
@@ -24,12 +24,14 @@ def Car(X, Y):
 black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
+dred = (200, 0, 0)
+dgreen = (0, 200, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
 
 #Message System
 def text_objects(text, font):
-    TextSurf = font.render(text, True, green)
+    TextSurf = font.render(text, True, blue)
     return TextSurf, TextSurf.get_rect()
 
 def message_display(listText):
@@ -43,9 +45,61 @@ def message_display(listText):
         i += 1
     pygame.display.update()
 
+#Blocks To Dodge
 def things(x, y, w, h, c):
     pygame.draw.rect(gameDisplay, c, [x, y, w, h])
-    
+
+#Score
+def things_dodged(count):
+    font = pygame.font.SysFont(None, 25)
+    text = font.render("Dodged: " + str(count), True, black)
+    gameDisplay.blit(text, (10,10))
+
+#Button
+def button(msg, x, y, w, h, i, a, action = None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    if x < mouse[0] < x+w and y < mouse[1] < y+h:
+        c = a
+        if click[0] == 1 and action != None:
+                action()
+
+    else:
+        c = i
+
+    pygame.draw.rect(gameDisplay, c, (x, y, w, h))
+
+    smallText =  pygame.font.Font("freesansbold.ttf", 20)
+    TextSurf, TextRec = text_objects(msg, smallText)
+    TextRec.center = ((x + (w/2), y + (h/2)))
+    gameDisplay.blit(TextSurf, TextRec)
+
+#Close window
+def closeWin():
+    pygame.quit()
+    quit()
+
+
+#Starting Screen
+def start_screen():
+    intro = True
+    scolor = dgreen
+    qcolor = dred
+    while intro:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                closeWin()
+
+        gameDisplay.fill(white)
+        style = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRec = text_objects("PyCarGame", style)
+        TextRec.center = ((display_width/2), (display_height/3))
+        gameDisplay.blit(TextSurf, TextRec)
+        
+        button("GO!", display_width / 3 - 50, 3 * display_height / 4 - 25, 100, 50, dgreen, green, GameLoop)
+        button("QUIT", 2 * display_width / 3 - 50, 3 * display_height / 4 - 25, 100, 50, dred, red, closeWin)
+        pygame.display.update()
+
 
 #Game Loop
 def GameLoop():
@@ -53,12 +107,14 @@ def GameLoop():
     sy = 0.7
     x = sx * display_width
     y = sy * display_height
-    tx = random.randrange(0, display_width)
-    ty = -600
-    ts = 7
     tw = 100
+    tx = random.randrange(0, display_width - tw)
+    ty = -600
+    ts = 4
+    
     th = 100
     crashed = False
+    score = 0
     while True:
         #Events
         for event in pygame.event.get():
@@ -68,8 +124,7 @@ def GameLoop():
                         GameLoop()
                         return
                     if event.key == pygame.K_q:
-                        pygame.quit()
-                        quit()
+                        closeWin()
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
@@ -78,9 +133,9 @@ def GameLoop():
         keys = pygame.key.get_pressed()
         if not crashed:
             if keys[pygame.K_LEFT]:
-                x += -5
+                x += -7
             if keys[pygame.K_RIGHT]:
-                x += 5
+                x += 7
         #Background
         gameDisplay.fill(white)
 
@@ -89,12 +144,17 @@ def GameLoop():
         if not crashed:
             ty += ts
         Car(x, y)
+        things_dodged(score)
         if x <= -2 or x + carWidth >= display_width:
             message_display(["You Crashed", "Press R", "To Restart"])
             crashed = True
         if ty > display_height:
             ty = 0 - th
-            tx = random.randrange(0, display_width)
+            tx = random.randrange(0, display_width - tw)
+            score += 1
+            ts += 1
+            tw += 2
+
 
         if y < ty+th and y + carNoSpoiler > ty:
             if x > tx and x < tx + tw or x + carWidth > tx and x + carWidth < tx + tw:
@@ -108,7 +168,7 @@ def GameLoop():
     
 
 
-
+start_screen()
 GameLoop()
 pygame.quit()
 quit()
